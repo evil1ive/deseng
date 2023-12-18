44f256adf70e9aa997cb03940c96eee10ad488e1
DamirLut <lutfrahmanovdamir3@gmail.com>
2023-12-18 16:28:36 +0500

    wip: auth modal

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from "@nestjs/common"
 
-import { AppConfigModule } from "@/common/config/config.module"
-import { DatabaseModule } from "@/common/database.module"
-import { UserModule } from "@/core/user/user.module"
+import { AppConfigModule } from "./common/config/config.module"
+import { DatabaseModule } from "./common/database.module"
+import { UserModule } from "./core/user/user.module"
 
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
+import { AuthModule } from "./core/auth/auth.module"
 
 @Module({
-    imports: [AppConfigModule, DatabaseModule, UserModule],
+    imports: [AppConfigModule, DatabaseModule, UserModule, AuthModule],
     controllers: [AppController],
     providers: [AppService],
 })
