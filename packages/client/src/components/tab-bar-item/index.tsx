4675f4c3a27053bc44be8da88f5b9a947873c76f
Виталий <46228376+LayXL@users.noreply.github.com>
2023-12-19 08:15:08 +0500

    refactor: renamed TabbarItemProps -> TabBarItemProps

--- packages/client/src/components/tab-bar-item/index.tsx
+++ packages/client/src/components/tab-bar-item/index.tsx
@@ -3,14 +3,14 @@ import cn from "classnames"
 
 import Style from "./tabbar.module.scss"
 
-type TabbarItemProps = {
+type TabBarItemProps = {
     label: string
     icon: ReactNode
     selected?: boolean
     onClick?: () => void
 }
 
-export const TabBarItem: FC<TabbarItemProps> = ({ icon, label, selected = false, onClick }) => {
+export const TabBarItem: FC<TabBarItemProps> = ({ icon, label, selected = false, onClick }) => {
     return (
         <button className={cn(Style.item, { [Style.item__selected]: selected })} onClick={onClick}>
             {icon}
